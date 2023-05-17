321aee984ecbccc188f1a48a883e9ccf8ff50b32
Test4 <4ucheba@mail.ru>
2023-05-17 21:14:43 +0300

    added test

---
+++ src/test/java/ru/netology/AppCardDeliveryTest.java
@@ -0,0 +1,53 @@
+package ru.netology;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.Keys;
+
+import java.time.Duration;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+import static com.codeborne.selenide.Condition.*;
+import static com.codeborne.selenide.Selenide.*;
+
+public class AppCardDeliveryTest {
+
+    @BeforeEach
+    void setUpTests() {
+        open("http://localhost:9999");
+    }
+
+    public String generateDate(int days) {
+        return LocalDate.now().plusDays(days).format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
+    }
+
+    String planningDate = generateDate(3);
+
+    @Test
+    void shouldSendForm() {
+        $("[data-test-id='city'] input").setValue("Москва");
+        $("[data-test-id='date'] input").doubleClick().sendKeys(Keys.BACK_SPACE);
+        $("[data-test-id='date'] input").setValue(planningDate);
+        $("[data-test-id='name'] input").setValue("Григорьева Юлия");
+        $("[data-test-id='phone'] input").setValue("+79991234567");
+        $("[data-test-id='agreement']").click();
+        $$("button").find(exactText("Забронировать")).click();
+        $("[data-test-id='notification']").shouldHave(text("Встреча успешно забронирована на " + planningDate), Duration.ofSeconds(15));
+        $("[data-test-id='notification']").shouldBe(visible);
+    }
+
+    @Test
+    void shouldSendFormWithDashInName() {
+        $("[data-test-id=city] input").setValue("Саратов");
+        $("[data-test-id=date] input").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.BACK_SPACE);
+        $("[data-test-id=date] input").setValue(planningDate);
+        $("[data-test-id=name] input").setValue("Иванов-Петров");
+        $("[data-test-id=phone] input").setValue("+79119871234");
+        $("[data-test-id=agreement]").click();
+        $$("button").find(exactText("Забронировать")).click();
+        $("[data-test-id='notification']").shouldHave(text("Встреча успешно забронирована на " + planningDate), Duration.ofSeconds(15));
+        $("[data-test-id='notification']").shouldBe(visible);
+    }
+
+
+}
